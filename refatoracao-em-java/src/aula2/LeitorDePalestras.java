9875467db28e7c06f535ab30f906ef3c902b79b9
lucarauj <lukas.martins193@gmail.com>
2023-08-30 11:43:32 -0300

    Add classe LeitorDePalestras antes de Internalizar metodo

---
+++ refatoracao-em-java/src/aula2/LeitorDePalestras.java
@@ -0,0 +1,24 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class LeitorDePalestras {
+
+    Scanner scanner = new Scanner(System.in);
+    private Palestra lerPalestra(Scanner scanner) {
+        String linha = scanner.nextLine();
+        return lerPalestra(linha);
+    }
+
+    private Palestra lerPalestra(String linha) {
+        int indiceDaDivisaoEntreNomeETempo = linha.lastIndexOf(' ');
+
+        String nome = linha.substring(0, indiceDaDivisaoEntreNomeETempo);
+        String tempoString = linha.substring(indiceDaDivisaoEntreNomeETempo + 1, linha.length() - 3);
+        TempoDeDuracao duracao = LIGHTING_STRING.contains(tempoString)
+                ? TempoDeDuracao.LIGHTING
+                : new TempoDeDuracao(Integer.valueOf(tempoString));
+
+        return new Palestra(nome, duracao);
+    }
+}
