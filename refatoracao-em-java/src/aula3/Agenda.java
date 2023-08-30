080cddbd30f127627e6b8942c71cb700355e6ce3
lucarauj <lukas.martins193@gmail.com>
2023-08-30 12:42:22 -0300

    Add DIvisao de Variavel temporaria

--- refatoracao-em-java/src/aula3/Agenda.java
+++ refatoracao-em-java/src/aula3/Agenda.java
@@ -19,10 +19,10 @@ public class Agenda {
         adicionarPalestrasSePossivel(umaSessaoDaTarde, listaDePalestras);
         adicionarPalestrasSePossivel(outraSessaoDaTarde, listaDePalestras);
 
-        String nomeDaTrilha = "Papo Reto";
-        Trilha tilha1 = new Trilha(nomeDaTrilha, umaSessaoDaManha, umaSessaoDaTarde);
-        nomeDaTrilha = "Digital";
-        Trilha trilha2 = new Trilha(nomeDaTrilha, outraSessaoDaManha, outraSessaoDaTarde);
+        final String nomeDaTrilha1 = "Papo Reto";
+        Trilha trilha1 = new Trilha(nomeDaTrilha1, umaSessaoDaManha, umaSessaoDaTarde);
+        final String nomeDaTrilha2 = "Digital";
+        Trilha trilha2 = new Trilha(nomeDaTrilha2, outraSessaoDaManha, outraSessaoDaTarde);
         return asList(trilha1, trilha2);
 
     }
