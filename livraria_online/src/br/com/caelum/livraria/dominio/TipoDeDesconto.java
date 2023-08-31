b93af9e87f64f2e7c04e2beaa2619c57290e5adc
lucarauj <lukas.martins193@gmail.com>
2023-08-31 17:24:12 -0300

    Refatoracao da classe TipoDeDesconto

--- livraria_online/src/br/com/caelum/livraria/dominio/TipoDeDesconto.java
+++ livraria_online/src/br/com/caelum/livraria/dominio/TipoDeDesconto.java
@@ -5,22 +5,27 @@ import org.javamoney.moneta.Money;
 import java.math.BigDecimal;
 
 public enum TipoDeDesconto {
-	
-	CUPOM_DE_DESCONTO, FIDELIDADE, NENHUM;
 
-	public Money getValor(Money subtotal) {
-		Money valor = Money.of(0, Livraria.reais);
-		
-		if(this.equals(TipoDeDesconto.CUPOM_DE_DESCONTO)) {
-			valor = subtotal.subtract(subtotal.with(quantia -> quantia.subtract(Money.of(5, Livraria.reais))));
+	CUPOM_DE_DESCONTO {
+		@Override
+		public Money getValor(Money subtotal) {
+			return subtotal.subtract(subtotal.with(quantia -> quantia.subtract(Money.of(5, Livraria.reais))));
 		}
-		else if(this.equals(TipoDeDesconto.FIDELIDADE)) {
-			valor = subtotal.subtract(subtotal.with(quantia -> quantia.multiply(BigDecimal.ONE.subtract(porcentagem(new BigDecimal(10))))));
+	}, FIDELIDADE {
+		@Override
+		public Money getValor(Money subtotal) {
+			return subtotal.subtract(subtotal.with(quantia -> quantia.multiply(BigDecimal.ONE.subtract(porcentagem(new BigDecimal(10))))));
 		}
-		return valor;
-	}
+		private BigDecimal porcentagem(BigDecimal fatorDeCalculo) {
+			return fatorDeCalculo.divide(BigDecimal.valueOf(100));
+		}
+	}, NENHUM {
+		@Override
+		public Money getValor(Money subtotal) {
+			return Money.of(0, Livraria.reais);
+		}
+	};
 
-	private BigDecimal porcentagem(BigDecimal fatorDeCalculo) {
-		return fatorDeCalculo.divide(BigDecimal.valueOf(100));
-	}
+	public abstract Money getValor(Money subtotal);
+
 }
