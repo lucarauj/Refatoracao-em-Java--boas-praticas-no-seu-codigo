487ac292ac538552ed08ef26bc7cd5c584739739
lucarauj <lukas.martins193@gmail.com>
2023-08-31 17:17:38 -0300

    Aplicacao de tecnica de Remocao de metodo da classe Desconto para o Enum TipoDeDesconto

--- livraria_online/src/br/com/caelum/livraria/dominio/TipoDeDesconto.java
+++ livraria_online/src/br/com/caelum/livraria/dominio/TipoDeDesconto.java
@@ -1,6 +1,26 @@
 package br.com.caelum.livraria.dominio;
 
+import org.javamoney.moneta.Money;
+
+import java.math.BigDecimal;
+
 public enum TipoDeDesconto {
 	
 	CUPOM_DE_DESCONTO, FIDELIDADE, NENHUM;
+
+	public Money getValor(Money subtotal) {
+		Money valor = Money.of(0, Livraria.reais);
+		
+		if(this.equals(TipoDeDesconto.CUPOM_DE_DESCONTO)) {
+			valor = subtotal.subtract(subtotal.with(quantia -> quantia.subtract(Money.of(5, Livraria.reais))));
+		}
+		else if(this.equals(TipoDeDesconto.FIDELIDADE)) {
+			valor = subtotal.subtract(subtotal.with(quantia -> quantia.multiply(BigDecimal.ONE.subtract(porcentagem(new BigDecimal(10))))));
+		}
+		return valor;
+	}
+
+	private BigDecimal porcentagem(BigDecimal fatorDeCalculo) {
+		return fatorDeCalculo.divide(BigDecimal.valueOf(100));
+	}
 }
